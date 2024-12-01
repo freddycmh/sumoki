d2082cc19d5b6336001e83498130eedf2ff640e7
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-12-01 17:04:30 -0500

    fixed the menu site

--- front/src/pages/Menu.jsx
+++ front/src/pages/Menu.jsx
@@ -27,7 +27,7 @@ export const Menu = () => {
   };
   return (
     <div className="flex flex-col items-center min-h-screen bg-black">
-      <Search />
+      {/* <Search /> */}
 
       <div className="w-full max-w-screen-lg px-4 mt-24">
         {Object.entries(cardData).map(([category, items]) => {
@@ -39,7 +39,7 @@ export const Menu = () => {
               <h1 className="text-2xl font-bold mb-6 underline text-white">
                 {category}
               </h1>
-              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 justify-items-center ">
+              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-items-center ">
                 {visibleItems.map((item) => (
                   <Card
                     key={item.id}
@@ -54,7 +54,7 @@ export const Menu = () => {
               {items.length > 4 && (
                 <div className="flex justify-center mt-4">
                   <button
-                    className="text-blue-500 underline"
+                    className="px-6 py-2 text-white bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 transition duration-300 ease-in-out shadow-md"
                     onClick={() => toggleCategory(category)}
                   >
                     {isExpanded ? "Show Less" : "Load More"}
